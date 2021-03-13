6ced1381b1336d3b1a92f845b47d244f0c882f3e
Trinity Christiana <trinitycterry@gmail.com>
2021-03-12 18:00:44 -0600

    setup js

---
+++ main.js
@@ -0,0 +1,4 @@
+// Section 1
+import runSectionTwo from "./section-02/index.js"
+
+runSectionTwo()
---
+++ section-02/index.js
@@ -0,0 +1,7 @@
+// B
+
+const runIt = () => {
+  console.log("Section 2: Big O Notation");
+}
+
+export default runIt
